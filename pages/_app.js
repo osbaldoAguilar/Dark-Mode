65390b2526314e7029322cdfbc2de08fdab2f656
osbaldoAguilar <osbaldoaguilar76@gmail.com>
2022-02-17 09:04:48 -0500

    DarkModeSimple

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import {useState} from 'react'
+import ThemeContextWrapper, {ThemeContext, themes} from '../themeContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [darkMode, setDarkMode] = useState(true)
+  return( <ThemeContextWrapper>
+        <ThemeContext.Consumer>
+
+          {({changeTheme}) => (
+            <button className='darkBtn' color='link' onClick={
+              () => {setDarkMode(!darkMode); changeTheme(darkMode ? themes.light : themes.dark);}
+              }>
+              <span>{darkMode ? 'Dark Mode 🌙': 'Light Mode ☀️'  }</span>
+            </button>
+          ) }
+        </ThemeContext.Consumer>
+      <Component {...pageProps} />
+  </ThemeContextWrapper> )
 }
 
 export default MyApp
